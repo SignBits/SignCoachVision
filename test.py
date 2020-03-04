61638ff65edeba872f50c0eb29edffc798127667
Michael Clancy <s1623705@sms.ed.ac.uk>
2020-03-04 11:52:58 -0500

    Commit

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+
+base_dir = os.path.join(os.getcwd(), 'split_dataset')
+
+IMAGE_SIZE = 200
+BATCH_SIZE = 64
+
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+test_generator = test_datagen.flow_from_directory(
+    os.path.join(base_dir, 'test'),
+    target_size = (IMAGE_SIZE, IMAGE_SIZE),
+    batch_size = BATCH_SIZE)
+
+model = tf.keras.models.load_model('models/first_train')
+
+true_labels = test_generator.classes
+probabilities = model.predict_generator(test_generator)
+
+y_pred = np.array([np.argmax(x) for x in probabilities])
+
+print(confusion_matrix(true_labels, y_pred))
+
+print(model.evaluate(tf.data.Dataset.from_generator(test_generator)))
---
+++ train.py
@@ -0,0 +1,109 @@
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+base_dir = os.path.join(os.getcwd(), 'split_dataset')
+
+IMAGE_SIZE = 200
+BATCH_SIZE = 64 
+
+#Load in the dataset, 
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, shear_range=0.4, zoom_range=0.4)
+val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    os.path.join(base_dir, 'train'),
+    target_size = (IMAGE_SIZE, IMAGE_SIZE),
+    batch_size = BATCH_SIZE)
+
+val_generator = val_datagen.flow_from_directory(
+    os.path.join(base_dir, 'val'),
+    target_size = (IMAGE_SIZE, IMAGE_SIZE),
+    batch_size = BATCH_SIZE)
+
+#Write the image labels out to labels.txt
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+
+with open('labels.txt', 'w') as f:
+  f.write(labels)
+
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                              include_top=False, 
+                                              weights='imagenet')
+
+base_model.trainable = True
+
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+	layer.trainable =  False
+
+
+model = tf.keras.Sequential([
+    base_model,    
+    tf.keras.layers.Dropout(0.8),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(29, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='SGD',
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
+
+
+epochs = 5
+
+#checkpoint_path = "checkpoints/first_train/cp.ckpt"
+#checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+#cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+#                                                 save_weights_only=True,
+#                                                 verbose=1)
+
+history = model.fit_generator(train_generator, 
+                    epochs=epochs, 
+                    validation_data=val_generator)
+
+model_path = './checkpoints/third_demo_train'
+model.save(model_path)
+
+print(history.history.keys())
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+plt.savefig("first_train.png")
