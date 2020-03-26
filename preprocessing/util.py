bd163469ace21a6176805695a35d334d550e58ec
wutangclancee <s1623705@sms.ed.ac.uk>
2020-03-26 12:08:29 +0000

    option to save frame every n frames ( to lower the amount of data we have)

--- preprocessing/util.py
+++ preprocessing/util.py
@@ -4,27 +4,31 @@ import os, ntpath
 
 class ImageWriter:
 
-    def __init__(self, id, out_file_loc):
+    def __init__(self, id, out_file_loc, write_frequency):
         self.out_file_loc = out_file_loc
         self.counter = 0
         self.id = id
+        self.write_frequency = write_frequency
 
     def write(self, frame):
-        cv.imwrite(f"{self.out_file_loc}/{self.id}_{self.counter}.bmp", frame)
+        if self.counter % self.write_frequency == 0:
+            cv.imwrite(f"{self.out_file_loc}/{self.id}_{self.counter}.bmp", frame)
+
         self.counter += 1
 
 
-def multiple_avi_to_bmp(in_file_dir: str, out_file_dir: str, preprocess_func=None):
+def multiple_avi_to_bmp(in_file_dir: str, out_file_dir: str, write_frequency, preprocess_func):
     for subdirs, dirs, files in os.walk(in_file_dir):
         for file in files:
-            avi_to_bmp(os.path.join(in_file_dir, file), out_file_dir, preprocess_func)
+            avi_to_bmp(os.path.join(in_file_dir, file), out_file_dir, write_frequency, preprocess_func)
 
 
-def avi_to_bmp(in_file_loc: str, out_file_dir: str, preprocess_func=None):
-    augment_video(in_file_loc, out_file_dir, preprocess_func, save_as_images=True)
+def avi_to_bmp(in_file_loc: str, out_file_dir: str, write_frequency, preprocess_func):
+    augment_video(in_file_loc, out_file_dir, write_frequency, preprocess_func, save_as_images=True)
 
 
-def augment_video(in_file_loc: str, out_file_dir: str, preprocess_func=None, fourcc='XVID', save_as_images=False):
+def augment_video(in_file_loc: str, out_file_dir: str, write_frequency, preprocess_func, fourcc='XVID',
+                  save_as_images=False):
     """This function takes a video file 'AVI' by default (change fourcc for different filetype) and augments
         each frame given the preprocessing_function. It then saves out_file_dir as either a single augmented video,
         or if save_as_images = True, saves the images to out_file_dir"""
@@ -49,7 +53,7 @@ def augment_video(in_file_loc: str, out_file_dir: str, preprocess_func=None, fou
     fps = int(capture.get(cv.CAP_PROP_FPS))
 
     if save_as_images:
-        out = ImageWriter(os.path.basename(in_file_loc), out_file_dir)
+        out = ImageWriter(os.path.basename(in_file_loc), out_file_dir, write_frequency)
     else:
         out = cv.VideoWriter(
             os.path.join(out_file_dir, ntpath.basename(in_file_loc)),
@@ -80,16 +84,17 @@ def augment_video(in_file_loc: str, out_file_dir: str, preprocess_func=None, fou
         out.release()
 
 
-def final(in_file_dir, out_file_dir, preprocess_func=None):
-
+def final(in_file_dir, out_file_dir, preprocess_func=None, write_frequency=4):
     for subdirs, dirs, files in os.walk(in_file_dir):
         for directory in dirs:
             multiple_avi_to_bmp(
                 in_file_dir=os.path.join(in_file_dir, directory),
                 out_file_dir=os.path.join(out_file_dir, directory),
-                preprocess_func=preprocess_func
+                preprocess_func=preprocess_func,
+                write_frequency=write_frequency
             )
 
 
 final("/Users/mclancy/Documents/sdp/preprocesstest/folder",
-      "/Users/mclancy/Documents/sdp/preprocesstest/folderout")
+      "/Users/mclancy/Documents/sdp/preprocesstest/folderout",
+      )
